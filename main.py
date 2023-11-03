2f64d0f603fb3379e9d261db077ea838f263b742
Gaurav Bhattacharya <81230208+gbiamgaurav@users.noreply.github.com>
2023-11-03 22:50:56 +0530

    data ingestion stage completed

---
+++ main.py
@@ -0,0 +1,13 @@
+
+from plant_seed import logger
+from plant_seed.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+   logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+        logger.exception(e)
+        raise e
