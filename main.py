2bb1dddb0d354212b8a7bdf8a699225338faec8a
Gaurav Bhattacharya <81230208+gbiamgaurav@users.noreply.github.com>
2023-11-06 20:24:44 +0530

    callbacks and training completed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from plant_seed import logger
 from plant_seed.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from plant_seed.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from plant_seed.pipeline.stage_03_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -18,6 +19,17 @@ try:
     logger.info(f"Stage: {STAGE_NAME} Started!")
     prepare_base_model = PrepareBaseModelTrainingPipeline()
     prepare_base_model.main()
+    logger.info(f"Stage: {STAGE_NAME} Completed!")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Training Stage"
+try:
+    logger.info(f"Stage: {STAGE_NAME} Started!")
+    model_training = ModelTrainingPipeline()
+    model_training.main()
     logger.info(f"Stage: {STAGE_NAME} Completed!")
 except Exception as e:
     logger.exception(e)
