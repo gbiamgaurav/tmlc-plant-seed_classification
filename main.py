581a9d8676f79af7f2e4e7b7ee6278d4df44cf86
Gaurav Bhattacharya <81230208+gbiamgaurav@users.noreply.github.com>
2023-11-07 23:24:13 +0530

    evaluation completed

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from plant_seed import logger
 from plant_seed.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from plant_seed.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from plant_seed.pipeline.stage_03_training import ModelTrainingPipeline
+from plant_seed.pipeline.stage_04_evaluation import ModelEvaluationPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -29,6 +30,17 @@ STAGE_NAME = "Model Training Stage"
 try:
     logger.info(f"Stage: {STAGE_NAME} Started!")
     model_training = ModelTrainingPipeline()
+    model_training.main()
+    logger.info(f"Stage: {STAGE_NAME} Completed!")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Evaluation Stage"
+try:
+    logger.info(f"Stage: {STAGE_NAME} Started!")
+    model_training = ModelEvaluationPipeline()
     model_training.main()
     logger.info(f"Stage: {STAGE_NAME} Completed!")
 except Exception as e:
--- src/plant_seed/config/configuration.py
+++ src/plant_seed/config/configuration.py
@@ -5,7 +5,8 @@ from plant_seed.utils.common import read_yaml, create_directories, save_json
 from plant_seed.entity.config_entity import (DataIngestionConfig,
                                              PrepareBaseModelConfig,
                                              PrepareCallbacksConfig,
-                                             TrainingConfig,)
+                                             TrainingConfig,
+                                             EvaluationConfig,)
 from plant_seed.constants import * 
                                                      
 
@@ -92,3 +93,16 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+
+    def get_evaluation_config(self) -> EvaluationConfig:
+        evaluation_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/",
+            mlflow_uri="https://dagshub.com/gbiamgaurav/tmlc-plant-seed_classification.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        
+        return evaluation_config
